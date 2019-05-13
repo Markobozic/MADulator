434452682f0260f5e4d3e6ca48311819a1dbb6cd
Marko <bozic@pdx.edu>
2019-05-13 16:23:02 -0700

    add skeleton for expression model

---
+++ src/expression.py
@@ -0,0 +1,69 @@
+class Expression:
+
+    def __init__(self):
+        pass
+
+    def eval(self):
+        pass
+
+
+class Value(Expression):
+
+    def __init__(self):
+        pass
+
+    def eval(self):
+        pass
+
+class Add(Expression):
+
+    def __init__(self):
+        pass
+
+    def eval(self):
+        pass
+
+
+class Sub(Expression):
+
+    def __init__(self):
+        pass
+
+    def eval(self):
+        pass
+
+
+class Mult(Expression):
+
+    def __init__(self):
+        pass
+
+    def eval(self):
+        pass
+
+
+class Div(Expression):
+
+    def __init__(self):
+        pass
+
+    def eval(self):
+        pass
+
+
+class Var(Expression):
+
+    def __init__(self):
+        pass
+
+    def eval(self):
+        pass
+
+
+class Shift(Expression):
+
+    def __init__(self):
+        pass
+
+    def eval(self):
+        pass
