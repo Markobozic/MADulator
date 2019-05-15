3d3f74797e1c965fbf04dd33e058e605272a9655
kououken <kououken@gmail.com>
2019-05-15 16:20:04 -0700

    Simple test program for keyboard in qt.

---
+++ src/tests/keyboard.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append("..")
+
+
+from pyqtgraph.Qt import QtCore
+import pyqtgraph as pg
+
+class KeyPressWindow(pg.GraphicsWindow):
+    sigKeyPress = QtCore.pyqtSignal(object)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def keyPressEvent(self, ev):
+        self.scene().keyPressEvent(ev)
+        self.sigKeyPress.emit(ev)
+
+
+def keyPressed(evt):
+    print("Key pressed")
+
+
+app = pg.mkQApp()
+win = KeyPressWindow()
+win.sigKeyPress.connect(keyPressed)
+pl = win.addPlot()
+pl.plot([x*x for x in range(-10,11)])
+
+
+app.exec_()
