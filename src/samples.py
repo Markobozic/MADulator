39c7c83ef1d3bd51b3d65d435f353a11793d5a12
Marko <bozic@pdx.edu>
2019-05-27 21:13:36 -0700

    add thread locking, remove unused imports and capitalize constants

--- src/samples.py
+++ src/samples.py
@@ -1,7 +1,7 @@
 from pyqtgraph.Qt import QtCore
 from expression import *
 from pyaudio import *
-import array
+import threading
 import numpy as np
 
 
@@ -9,12 +9,14 @@ class Samples:
 
     def __init__(self, waveform_signal: QtCore.pyqtSignal = None, spectrogram_signal: QtCore.pyqtSignal = None):
         self.expression = Expression()
+        self.lock = threading.Lock()
         self.waveform_signal = waveform_signal
         self.spectrogram_signal = spectrogram_signal
         self.samples = []
         self.position = 0
 
     def pyaudio_callback(self, in_data, frame_count, time_info, status) -> (str, int):
+        self.lock.acquire()
         sound_samples = []
         for i in range(self.position, self.position + frame_count):
             value = self.expression.eval(i) % 256
@@ -22,18 +24,23 @@ class Samples:
             sound_samples.append(chr(value))
         data = ''.join(sound_samples)
         self.position += frame_count
-        if (self.waveform_signal is not None and self.spectrogram_signal is not None) and len(self.samples) >= 1024:
-            self.waveform_signal.emit(self.samples[-1024:])
-            self.spectrogram_signal.emit(np.asarray(self.samples[-1024:]))
+        try:
+            if (self.waveform_signal is not None and self.spectrogram_signal is not None) and len(self.samples) >= 1024:
+                self.waveform_signal.emit(self.samples[-1024:])
+                self.spectrogram_signal.emit(np.asarray(self.samples[-1024:]))
+        finally:
+            self.lock.release()
         return data, paContinue
 
     def get_expression(self) -> Expression:
         return self.expression
 
     def set_expression(self, expression: Expression) -> None:
+        self.lock.acquire()
         self.expression = expression
         self.reset()
 
     def reset(self) -> None:
         self.position = 0
+        self.lock.release()
         self.samples.clear()
