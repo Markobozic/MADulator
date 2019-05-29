WAV file updates but wrong math calculation

src/samples.py
@@ -1,8 +1,10 @@
-from pyqtgraph.Qt import QtCore
+from pyqtgraph.Qt import QtCore, QtGui
 from expression import *
 from pyaudio import *
 import array
 import numpy as np
+import wave
+import struct
 
 
 class Samples:
@@ -27,11 +29,20 @@ class Samples:
             self.spectrogram_signal.emit(np.frombuffer(array.array('B', self.samples[-1024:]).tobytes(), 'int8'))
         return data, paContinue
 
-    def generate_samples(self, num_samples: int) -> None:
-        sound_samples = []
+    def generate_samples_and_write(self, filename: str, duration: int, rate: int) -> None:
+        num_samples = duration * rate
+        wave_file = wave.open(filename, 'wb')
+        wave_file.setnchannels(2)
+        wave_file.setsampwidth(2)
+        wave_file.setframerate(rate)
         for i in range(num_samples):
-            value = self.expression.eval(i) % 256
-            self.samples.append(value)
+            # Need to correct the math
+            one = self.expression.eval(i) % 32767
+            two = self.expression.eval(i) % 32767
+            sample = struct.pack('<hh', one, two)
+            wave_file.writeframesraw(sample)
+        wave_file.writeframes(b'')
+        wave_file.close()
 
     def get_samples(self) -> list:
         return self.samples